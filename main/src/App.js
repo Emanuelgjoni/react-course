5f31f43290eecc5bbcdaf28ef430dd2e793ed0c0
Emanuel Gjoni <emanuel.gjoni@eurtostep.it>
2024-03-28 12:10:05 +0100

    Add router

--- main/src/App.js
+++ main/src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import About from './components/About';
+
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
   const addTask = async (task) => {
     const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = { id, ...task };
 
     const res = await fetch(`http://localhost:5000/tasks`, {
       method: 'Post',
@@ -18,10 +20,10 @@ function App() {
       },
       body: JSON.stringify(task)
     })
-    const data =  await res.json();
+    const data = await res.json();
     setTasks([...tasks, data])
     setShowAddTask(false)
-  
+
   }
   const onDeleteTask = async (id) => {
 
@@ -29,19 +31,19 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
   const setReminder = async (id) => {
-    const task= await fetchTask(id)
+    const task = await fetchTask(id)
 
-    const updateTask={...task,reminder:!task.reminder}
+    const updateTask = { ...task, reminder: !task.reminder }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
-      method:'Put',
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'Put',
       headers: {
         'Content-type': 'application/json'
       },
       body: JSON.stringify(updateTask)
     })
-    const data =await res.json()
-   
+    const data = await res.json()
+
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
 
   }
@@ -66,13 +68,25 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header title="My Task Tracker" showTask={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={onDeleteTask}  setReminder={setReminder} /> : <h3>No task Found</h3>}
-      <Footer/>
-    </div>
+    <Router>
+      <div className="container">
+        <Header title="My Task Tracker" showTask={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={onDeleteTask} setReminder={setReminder} /> : <h3>No task Found</h3>}
+                <Footer/>
+              </>
+            }
+          />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      
+      </div>
+    </Router>
   );
 }
 
---
+++ main/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div style={{ "display": "flex",
+            "align-content": "space-between",
+            "flex-direction": "column",
+            "justify-content": "space-between",
+            "align-items": "center" }}>
+            <h2>This is about page</h2>
+            <a href="/">Go to Home pagse</a>
+        </div>
+    )
+}
+
+export default About
